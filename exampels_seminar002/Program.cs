1b18d5eea92f714463d1a11c506cd59e263e387f
Natali <natalia.svetenkova@gmail.com>
2022-08-06 18:51:16 +0300

    seminar002

---
+++ exampels_seminar002/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("число a = ");
+int numberA = int.Parse( Console.ReadLine());
+Console.Write ("число b = ");
+int numberB = int.Parse( Console.ReadLine());
+
+
+if ( numberA == numberB*numberB )
+{
+Console.WriteLine ("да");
+}
+else {
+    Console.WriteLine("нет");
+}
